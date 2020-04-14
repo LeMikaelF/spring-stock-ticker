6b84306f45ca89802b5cee401216631d122350c1
Mikaël Francoeur <mikael@mikaelfrancoeur.com>
2020-04-13 23:06:26 -0400

    delete unused Environment

--- src/main/java/com/mikaelfrancoeur/demostockticker/DemoStockTickerApplication.java
+++ src/main/java/com/mikaelfrancoeur/demostockticker/DemoStockTickerApplication.java
@@ -2,7 +2,6 @@ package com.mikaelfrancoeur.demostockticker;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,18 +9,15 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @PropertySource(value = "keys.properties")
 public class DemoStockTickerApplication {
 
-    final Environment env;
     Logger logger = LoggerFactory.getLogger(DemoStockTickerApplication.class);
 
-    public DemoStockTickerApplication(Environment env) {
-        this.env = env;
+    public DemoStockTickerApplication() {
     }
 
     public static void main(String[] args) {
