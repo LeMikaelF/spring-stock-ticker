e2e06b59848352fc1461ceb3092ae82a52bc2c5c
Mikaël Francoeur <mikael@mikaelfrancoeur.com>
2020-04-13 23:43:59 -0400

    Factor out REST URL and key into property files

--- src/main/java/com/mikaelfrancoeur/demostockticker/StockClient.java
+++ src/main/java/com/mikaelfrancoeur/demostockticker/StockClient.java
@@ -4,31 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
 @Component
 public class StockClient {
 
-    private final String key;
-    @Autowired
-    private RestOperations restOperations;
+    private final RestOperations restOperations;
     private Logger logger = LoggerFactory.getLogger(StockClient.class);
+    @Value("${stockapi.key}") private String key;
+    @Value("${stockapi.getstockurl}") private String getStockUrl;
 
-    public StockClient(@Value("${FinnHubKey}") String key) {
-        this.key = key;
-
+    @Autowired
+    public StockClient(RestOperations restOperations) {
+        logger.info(this.key);
+        this.restOperations = restOperations;
     }
 
 
     StockResponse getStockInfo(String symbol) {
-        // Documentation de l'API:
-        // https://finnhub.io/docs/api#quote
-
-        String url = String.format("https://finnhub.io/api/v1/quote?symbol=%s&token=%s", symbol,
-                key);
-        return restOperations.getForEntity(url, StockResponse.class).getBody();
+        logger.info("URL from properties and injection: " + getStockUrl);
+        return restOperations.getForEntity(getStockUrl, StockResponse.class, symbol, key).getBody();
     }
 
 }
